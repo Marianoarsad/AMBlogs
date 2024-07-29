66903363dec980bde9bf45f0380706e9a0217a8e
Marianoarsad <arsadmariano@yahoo.com.ph>
2024-07-29 11:02:23 +0800

    created dynamic user blogs

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+var postArray = [];
+var postDB = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -16,9 +18,35 @@ app.get("/create", (req, res) => {
 });
 
 app.get("/blogs", (req, res) => {
-    res.render("blogs.ejs");
+
+    if (postDB.length === 0) {
+        res.render("blogs.ejs")
+    }   else {
+        res.render("blogs.ejs", {
+            postDB: postDB,
+        })
+    }
+
+});
+
+app.post("/submit", (req, res) => {
+
+    postArray = [req.body["fname"] + req.body["lname"], req.body["title"], req.body["content"]];
+    postDB.push(postArray);
+
+    res.render("./blogs.ejs", {
+        postDB: postDB
+    });
+
+    numberOfPosts++;
+});
+
+app.put("/update", (req, res) => {
+    res.console.log('you want to update')
 });
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 });
+
+
